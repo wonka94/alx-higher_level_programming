437f1eefb1c84e1fb9dc0637d5c46e2ad320e368
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-09 22:54:02 +0000

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for letter in range(ord('a'), ord('z')+1):
-        if chr(c) == chr(letter):
-            return True
-        else:
-            return False
+    if ord(c) > 96
+        return True
+    else:
+        return False
