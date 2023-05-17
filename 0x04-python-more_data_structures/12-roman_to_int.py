bc15e3e6fc7d04f9d254441bf1b3b80bfe976ff6
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-17 23:16:23 +0000

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or len(roman_string) == 0:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
     else:
         result = 0
