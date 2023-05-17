f7a557b7981e6be002db7531fafe120b1492d5ef
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-17 23:12:34 +0000

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,9 @@ def roman_to_int(roman_string):
     else:
         result = 0
         rn = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        for c in roman_string:
+        for c in roman_string.upper():
             if c in rom_values.keys():
-                result += rn[c]
+                result += int(rn[c])
             else:
                 return 0
         return int(result)
