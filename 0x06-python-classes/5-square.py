6ce13d698e230e51f40dfa7e48be51ffcedf201d
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-05-30 20:12:00 +0000

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -35,6 +35,8 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
+        if self.__size == 0:
+            print("")
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
