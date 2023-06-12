748a7cc91c3ba0226688a9baf9e77e56ed6fa601
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-06-12 10:07:52 +0000

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module for a lookup function"""
+
+
+def lookup(obj):
+    """
+        Returns the list of available attributes and methods
+        of an object
+    """
+    return dir(obj)
