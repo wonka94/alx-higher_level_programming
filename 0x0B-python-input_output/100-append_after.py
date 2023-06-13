845db87fbb2e5dd192ef7381b213fad677f13894
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-06-13 13:47:05 +0000

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ append_after module """
 
+
 def append_after(filename="", search_string="", new_string=""):
     """Inserts a line of text to a file, after each line
        containing a specific string."""
