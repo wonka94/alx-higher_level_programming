b89a2a98a962ae447f816078a461e610a315d6df
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-06-20 07:37:11 +0000

    Code fixes

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -92,6 +92,8 @@ class Base:
             dummy = cls(1, 1)
         elif cls.__name__ == "Square":
             dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
