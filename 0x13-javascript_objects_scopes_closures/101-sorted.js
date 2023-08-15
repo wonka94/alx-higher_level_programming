cb9f437c81ba5cbb275190e02336931360d66af4
Wilfred Ashiagbor <wilfredonii45@gmail.com>
2023-08-15 23:14:18 +0000

    advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const newDict = {};
+for (const [key, value] of Object.entries(dict)) {
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
+  newDict[value].unshift(key);
+}
+console.log(newDict);
